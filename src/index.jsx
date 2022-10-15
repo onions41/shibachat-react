8fbc93d861c7382046329d02e71b5f295020a8d7
onions41 <kblee89@outlook.com>
2022-10-15 00:39:21 -0700

    Moved ApolloProvider to src/index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ApolloProvider } from '@apollo/client'
+import apolloClient from './graphql/apolloClient'
 import CssBaseline from '@mui/material/CssBaseline'
+
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <ApolloProvider client={apolloClient}>
+      <CssBaseline />
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 )
