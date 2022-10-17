000f8cae09b3e11adbad4b47dcd1d5ad553a2a94
onions41 <kblee89@outlook.com>
2022-10-16 21:11:28 -0700

    Created Copyright.jsx

---
+++ src/components/Copyright.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Typography, Link } from '@mui/material'
+
+export default function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  )
+}
