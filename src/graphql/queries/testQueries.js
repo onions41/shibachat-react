2da27f99f0129645636311ca8df639a6d4640332
onions41 <kblee89@outlook.com>
2022-10-17 16:28:06 -0700

    Added 2 test queries

---
+++ src/graphql/queries/testQueries.js
@@ -0,0 +1,15 @@
+import { gql } from '@apollo/client'
+
+const PROTECTED = gql`
+  query Protected {
+    protected
+  }
+`
+
+const UNPROTECTED = gql`
+  query Unprotected {
+    unprotected
+  }
+`
+
+export { PROTECTED, UNPROTECTED }
