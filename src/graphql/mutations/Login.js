a97add433f5f17d4db6e4fcc065ca108e93dc8d4
onions41 <kblee89@outlook.com>
2022-10-16 21:43:21 -0700

    Created login mutation

---
+++ src/graphql/mutations/Login.js
@@ -0,0 +1,11 @@
+import { gql } from '@apollo/client'
+
+const LOGIN = gql`
+  mutation Login($nickname: String!, $password: String!) {
+  login(nickname: $nickname, password: $password) {
+    accessToken
+  }
+}
+`
+
+export default LOGIN
