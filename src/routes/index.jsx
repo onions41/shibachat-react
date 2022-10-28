b273606f6227f0d61426dab9aa68bf4c6b685523
onions41 <kblee89@outlook.com>
2022-10-28 16:25:55 -0700

    Set routes in routes/index.js

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,32 @@
+// Module imports
 import React from 'react'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
 
 // Routes
-import Chat from './Chat'
+import Home from './Home'
+import Register from './Register'
 
 export default function Router() {
   return (
-    // TODO: Wrap in a BrowserRouter
-    <Chat />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        {/* <Route path="/teams" element={<RequiresAuth><ViewTeams /></RequiresAuth>} />
+        <Route path="/teams/:teamId" element={<RequiresAuth><ViewTeams /></RequiresAuth>} />
+        <Route path="/teams/:teamId/:chatMode" element={<RequiresAuth><ViewTeams /></RequiresAuth>} />
+        <Route path="/teams/:teamId/:chatMode/:subjectId" element={<RequiresAuth><ViewTeams /></RequiresAuth>} />
+        <Route
+          path="/teams/new"
+          element={
+            <RequiresAuth><CreateTeam /></RequiresAuth>
+          }
+        /> */}
+      </Routes>
+    </BrowserRouter>
   )
 }
