2fbff6a533c3dfd380231cd95407c48708e6a8a5
onions41 <kblee89@outlook.com>
2022-10-28 18:39:15 -0700

    Wrote initial auth logic in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,55 @@
-import React from 'react'
+// Module imports
+import React, {
+  useEffect,
+  useState
+} from 'react'
 import Routes from './routes'
 
-function App() {
-  // TODO: Add code for initial App mounting authentication
-  // All state must be held below this level.
-  // Ie, I don't want App component and the components above to re-render too often
+// Internal imports
+import { useDispatch } from 'react-redux'
+import { loginAction } from './store'
+
+/**
+ * App is wrapped with StrictMode, ApolloProvider, ReduxProvider, and CSSBaseline (MUI) in index.js
+ * App first tries to make contact with /refresh-token. If no contact can be made, App will not render
+ * the rest of the application; it will just render an error page.
+ * If a connection could be made with the server, the server either sent back new auth tokens or it didn't.
+ * If it did, set isLoggedInState to true (which also store the access token),
+ * then render the rest the application.
+ * If it did not send back new tokens, do nothing (isLoggedIn state is false initally),
+ * then render the rest of the application.
+ */
+export default function App() {
+  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(true)
+  const [fetchError, setFetchError] = useState(false)
+
+  useEffect(() => {
+    fetch(process.env.REACT_APP_REFRESH_SERVER_URL, {
+      method: 'POST',
+      credentials: 'include'
+    }).then(async (response) => {
+      const { accessToken } = await response.json()
+      if (accessToken) {
+        dispatch(loginAction(accessToken))
+      }
+      setLoading(false)
+    }).catch(() => { // Fetch only throws network errors. Ie, no connection could be made.
+      setFetchError(true)
+    })
+  }, [])
+
+  // TODO: Better error screen
+  if (fetchError) {
+    return <div>Could not reach the internet</div>
+  }
+
+  // TODO: Better loading screen
+  if (loading) {
+    return <div>Loading while App tries to log in</div>
+  }
+
   return (
     <Routes />
   )
 }
-
-export default App
