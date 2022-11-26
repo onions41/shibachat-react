a1183f1a99b93c79fbc606c166552ce8dce98669
onions41 <kblee89@outlook.com>
2022-11-25 22:00:41 -0800

    Changed to double quotes

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -10,6 +10,14 @@ import {
 } from "@mui/material"
 import {
   Folder as FolderIcon,
+  // import { render, screen } from "@testing-library/react"
+  // import App from "./App"
+  // test("renders learn react link", () => {
+  //   render(<App />)
+  //   const linkElement = screen.getByText(/learn react/i)
+  //   expect(linkElement).toBeInTheDocument()
+  // })
+
   DoNotDisturbAltOutlined as NoIcon
 } from "@mui/icons-material"
 
