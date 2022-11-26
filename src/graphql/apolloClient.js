b5f5cdb80e519990f5398c68d3c2d8c0d3ac6548
onions41 <kblee89@outlook.com>
2022-11-25 22:11:58 -0800

    Changed to double quotes

--- src/graphql/apolloClient.js
+++ src/graphql/apolloClient.js
@@ -1,19 +1,11 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  from
-} from '@apollo/client'
+import { ApolloClient, InMemoryCache, from } from "@apollo/client"
 
-import httpLink from './links/httpLink'
-import authLink from './links/authLink'
-import tokenRefreshLink from './links/tokenRefreshLink'
+import httpLink from "./links/httpLink"
+import authLink from "./links/authLink"
+import tokenRefreshLink from "./links/tokenRefreshLink"
 
 // Combines some of the links
-const additiveLink = from([
-  tokenRefreshLink,
-  authLink,
-  httpLink
-])
+const additiveLink = from([tokenRefreshLink, authLink, httpLink])
 
 // Creates client out of links
 const apolloClient = new ApolloClient({
