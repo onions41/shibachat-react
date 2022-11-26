580c2592bcb00501a337974f33367ec23d2f8f09
onions41 <kblee89@outlook.com>
2022-11-26 13:46:00 -0800

    Removed keyboard smoosh error

--- src/routes/Chat.jsx
+++ src/routes/Chat.jsx
@@ -21,16 +21,15 @@ export default function Chat() {
   }
 
   if (loading) {
-    return <div>Loading...</div>import React from "react"
+    return <div>Loading...</div>
     // import { render, screen } from "@testing-library/react"
     // import App from "./App"
-    
+
     // test("renders learn react link", () => {
     //   render(<App />)
     //   const linkElement = screen.getByText(/learn react/i)
     //   expect(linkElement).toBeInTheDocument()
     // })
-    
   }
 
   if (error) {
