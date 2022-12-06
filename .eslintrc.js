c41656df4cfe12f7ea52bdb8312259d15506a3e3
onions41 <kblee89@outlook.com>
2022-12-05 22:48:34 -0800

    Rule: Don't require newline on ternary

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,8 @@ module.exports = {
     "arrow-parens": ["error", "always"],
     "no-unused-vars": 1,
     "react/prop-types": 0,
-    "no-console": 1
+    "no-console": 1,
+    "multiline-ternary": 0
   },
   globals: {
     describe: "readonly",
