b8720330075f3a8a0d3a306f9df568145591ad34
onions41 <kblee89@outlook.com>
2023-02-07 18:35:31 -0800

    wsLink provided to apolloClient through splitlink

--- src/graphql/apolloClient.js
+++ src/graphql/apolloClient.js
@@ -1,15 +1,31 @@
-import { ApolloClient, InMemoryCache, from } from "@apollo/client"
+// Module imports
+import { ApolloClient, InMemoryCache, from, split } from "@apollo/client"
+import { getMainDefinition } from "@apollo/client/utilities"
 
+// Internal imports
 import httpLink from "./links/httpLink"
 import authLink from "./links/authLink"
 import tokenRefreshLink from "./links/tokenRefreshLink"
+import wsLink from "./links/wsLink"
 
 // Combines some of the links
 const additiveLink = from([tokenRefreshLink, authLink, httpLink])
 
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    )
+  },
+  wsLink,
+  additiveLink
+)
+
 // Creates client out of links
 const apolloClient = new ApolloClient({
-  link: additiveLink,
+  link: splitLink,
   cache: new InMemoryCache({
     typePolicies: {
       FriendRequest: {
