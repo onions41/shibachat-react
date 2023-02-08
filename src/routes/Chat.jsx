ba9556cd3cdc04ddcaa5c7976c2e2ec739966e2d
onions41 <kblee89@outlook.com>
2023-02-07 20:54:59 -0800

    Added subscribeToMore feature

--- src/routes/Chat.jsx
+++ src/routes/Chat.jsx
@@ -9,27 +9,57 @@ import { useLazyQuery } from "@apollo/client"
 
 // Internal imports
 import MESSAGES from "graphql/queries/Messages"
+import NEW_MESSAGE from "graphql/subscriptions/NewMessage"
 import MessageCard from "components/MessageCard"
 
 export default function Chat() {
   // Hooks
-  const { subjectId } = useParams()
+  const { subjectId: subjectIdString } = useParams()
+  const subjectId = parseInt(subjectIdString, 10) // subjectId was a string, parsed into int
 
-  const [fetchMessages, { called, loading, error, data }] = useLazyQuery(
-    MESSAGES,
-    {
+  const [fetchMessages, { called, loading, error, data, subscribeToMore }] =
+    useLazyQuery(MESSAGES, {
       fetchPolicy: "network-only"
-    }
-  )
-
-  // Runs only the first time, and then only after change to subjectId
-  useEffect(() => {
-    fetchMessages({
-      variables: {
-        friendId: parseInt(subjectId, 10)
-      }
     })
+
+  // Calls the query each time the subjectId changes if subjectId exists
+  useEffect(() => {
+    if (subjectId) {
+      fetchMessages({
+        variables: {
+          friendId: subjectId
+        }
+      })
+    }
   }, [subjectId])
+
+  // Unsubscribes then Subscribes each time data changes.
+  // (data changes when data is successfully fetched)
+  // The first time data is defined, this effect runs, subscribing.
+  // Then, each time data is redefined, effect cleanup runs (unsubscribe)
+  // then the effect runs (subscribe). When finally navigating away, cleanup will run.
+  useEffect(() => {
+    if (data) {
+      // Once all messages have been queried, subscribe.
+      const unsubscribe = subscribeToMore({
+        document: NEW_MESSAGE,
+        variables: { friendId: subjectId },
+        // ctrl+click updateQuery to see signature. Dont bother checking docs, it only adds confusion.
+        updateQuery(prev, { subscriptionData }) {
+          // If the subscription does not fetch a new message return the previous query result
+          if (!subscriptionData?.data) return prev
+          // Query object must be returned immutably
+          return {
+            ...prev,
+            messages: [...prev.messages, subscriptionData.data.newMessage]
+          }
+        }
+      })
+      return () => {
+        unsubscribe()
+      }
+    }
+  }, [data])
 
   // End of hooks
 
@@ -43,16 +73,17 @@ export default function Chat() {
     )
   }
 
-  if (!called) {
-    return <div style={{ width: "100%" }}>Not called yet</div>
+  // Not called yet. This screen should never last very long as query will be called in the effect immediately after, but
+  // is required coordinate the query status to the renders.
+  if (!called) { // TODO. Just show background in production.
+    return <div style={{ width: "100%" }}>Query not called yet</div>
   }
 
-  if (loading) {
+  if (loading) { // TODO. Show loading animation.
     return <div style={{ width: "100%" }}>Loading the Messages query</div>
   }
 
-  if (error) {
-    console.log("***Messages query error: ", error)
+  if (error) { // TODO. Display via toast
     return (
       <div style={{ width: "100%" }}>
         Messages query threw an error. Logged to console.
@@ -61,6 +92,9 @@ export default function Chat() {
   }
 
   // data was fetched successfully
+
+  // messages query always returns an array, if its empty that means
+  // that the user does not exist, or there are just no messages between the users
   if (!data.messages.length) {
     return (
       <div style={{ width: "100%" }}>
@@ -69,11 +103,14 @@ export default function Chat() {
     )
   }
 
-  return (
+  return ( // TODO. Make it look good.
     <Container>
       <List dense={false}>
         {data.messages.map((message) => (
-          <MessageCard key={`message-id-${message.id}`} message={message} />
+          <MessageCard
+            key={`message-id-${message.id}`}
+            message={message}
+          />
         ))}
       </List>
     </Container>
