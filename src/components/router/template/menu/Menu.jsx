382510182ba863fe5d53f0ab09fde6eff0fb391e
onions41 <kblee89@outlook.com>
2023-02-09 19:17:41 -0800

    Menu moved into Template folder

---
+++ src/components/router/template/menu/Menu.jsx
@@ -0,0 +1,10 @@
+// MUI
+import Box from "@mui/material/Box"
+
+export default function Menu() {
+  return (
+    <Box sx={{ width: "100%", height: 90, backgroundColor: "sienna" }}>
+      This will be the menu content right in here
+    </Box>
+  )
+}
