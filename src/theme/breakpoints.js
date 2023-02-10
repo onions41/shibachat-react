f99134ccfdc2e597d86da5dc3ab116d350f6dac6
onions41 <kblee89@outlook.com>
2023-02-09 16:57:41 -0800

    Not using custom breakpoints

--- src/theme/breakpoints.js
+++ src/theme/breakpoints.js
@@ -1,13 +1,22 @@
-const breakpoints = {
-  values: {
-    xxs: 0, // 0 to 359 is xxs
-    xs: 360, // 360 to 479 sx
-    sm: 480, // 480 to 639 is mobile
-    md: 640, // 640 or larger is desktop
-    lg: 1024,
-    xl: 1280,
-    xxl: 1440 // Max width for the content on desktop
-  }
-}
+// This file is not used. MUI default breakpoints are used.
 
-export default breakpoints
+// Defaults
+// xs: 0 to 599
+// sm, 600 to 899
+// md, 900 to 1199
+// lg, 1200 to 1535
+// xl, 1536 and higher
+
+// const breakpoints = {
+//   values: {
+//     xxs: 0, // 0 to 359 is xxs
+//     xs: 360, // 360 to 479 sx
+//     sm: 480, // 480 to 639 is mobile
+//     md: 640, // 640 or larger is desktop
+//     lg: 1024,
+//     xl: 1280,
+//     xxl: 1440 // Max width for the content on desktop
+//   }
+// }
+
+// export default breakpoints
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,13 +1,6 @@
-// Module imports
 import { createTheme } from "@mui/material/styles"
-
-// Internal imports
 import palette from "./palette"
-import breakpoints from "./breakpoints"
 
-const theme = createTheme({
-  palette,
-  breakpoints
-})
+const theme = createTheme({ palette })
 
 export default theme
