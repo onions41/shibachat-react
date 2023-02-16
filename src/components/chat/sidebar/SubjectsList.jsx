ac399e01f80a4d870b7475066efe65489bbe03df
onions41 <kblee89@outlook.com>
2023-02-15 20:42:56 -0800

    Displays latest messages

--- src/components/chat/sidebar/SubjectsList.jsx
+++ src/components/chat/sidebar/SubjectsList.jsx
@@ -4,17 +4,22 @@ import Stack from "@mui/material/Stack"
 import Typography from "@mui/material/Typography"
 
 // Internal imports
-import FriendCard from "./FriendCard"
+import SubjectCard from "./SubjectCard"
 
-export default function FriendsList({ me }) {
+export default function SubjectsList({ me }) {
+  // Filter me.friends, which is an array, by latestMessageWithMe me being null or not
+  const messagedFriends = me.friends.filter((friend) => friend.latestMessageWithMe)
+  // Sort the resulting array by friend.latestMessageWithMe
+  const messageFriendsDesc = messagedFriends.sort((later, earlier) => parseInt(later, 10) - parseInt(earlier, 10))
+
   return (
     <List dense={false}>
       {/* List header */}
       <ListHeader />
       {/* List items */}
-      {me.friends.map((friend) => (
-        <FriendCard
-          friend={friend}
+      {messageFriendsDesc.map((friend) => (
+        <SubjectCard
+          subject={friend}
           // cancelFReqLoading={cancelFReqLoading}add
           key={`friend-${friend.id}`}
         />
@@ -32,7 +37,7 @@ function ListHeader() {
       spacing={1}
       sx={{ bgcolor: "primary.dark", px: 0.5, mb: 1 }}
     >
-      <Typography sx={{ pl: 1.4, pt: 0.6 }}>Friend inus!</Typography>
+      <Typography sx={{ pl: 1.4, pt: 0.6 }} component="h1">Messages</Typography>
     </Stack>
   )
 }
