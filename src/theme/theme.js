c2b825119f45568bdc44de9a84244b278c1ba795
onions41 <kblee89@outlook.com>
2023-02-16 21:35:51 -0800

    formatted h1 in theme

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,6 +1,7 @@
 import { createTheme } from "@mui/material/styles"
 import palette from "./palette"
+import typography from "./typography"
 
-const theme = createTheme({ palette })
+const theme = createTheme({ palette, typography })
 
 export default theme
---
+++ src/theme/typography.js
@@ -0,0 +1,8 @@
+const typography = {
+  h1: {
+    fontWeight: 700,
+    fontSize: "1.3rem"
+  }
+}
+
+export default typography
