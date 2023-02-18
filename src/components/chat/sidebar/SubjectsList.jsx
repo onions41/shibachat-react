59bd0d20cf7cc5708a30737fd890aa417100c923
onions41 <kblee89@outlook.com>
2023-02-17 19:11:43 -0800

    Removed the list header

--- src/components/chat/sidebar/SubjectsList.jsx
+++ src/components/chat/sidebar/SubjectsList.jsx
@@ -1,22 +1,17 @@
 // MUI
 import List from "@mui/material/List"
-import Stack from "@mui/material/Stack"
-import Typography from "@mui/material/Typography"
 
 // Internal imports
 import SubjectCard from "./SubjectCard"
 
 export default function SubjectsList({ me }) {
-  // Filter me.friends, which is an array, by latestMessageWithMe me being null or not
+  // Filters friends by whether or not there are messages between user and friend
+  // then sorts the results in descending order by date.
   const messagedFriends = me.friends.filter((friend) => friend.latestMessageWithMe)
-  // Sort the resulting array by friend.latestMessageWithMe
   const messageFriendsDesc = messagedFriends.sort((later, earlier) => parseInt(later, 10) - parseInt(earlier, 10))
 
   return (
     <List dense={false}>
-      {/* List header */}
-      <ListHeader />
-      {/* List items */}
       {messageFriendsDesc.map((friend) => (
         <SubjectCard
           subject={friend}
@@ -25,19 +20,5 @@ export default function SubjectsList({ me }) {
         />
       ))}
     </List>
-  )
-}
-
-function ListHeader() {
-  return (
-    <Stack
-      direction="row"
-      alignItems="center"
-      justifyContent="space-between"
-      spacing={1}
-      sx={{ bgcolor: "primary.dark", px: 0.5, mb: 1 }}
-    >
-      <Typography sx={{ pl: 1.4, pt: 0.6 }} component="h1">Messages</Typography>
-    </Stack>
   )
 }
