d2b24fdd355c76a3d5cc85f66f4ff6e3dd32d1f5
onions41 <kblee89@outlook.com>
2023-02-25 00:38:38 -0800

    TopBar placeholder

---
+++ src/components/contacts/TopBar.jsx
@@ -0,0 +1,13 @@
+import Box from "@mui/material/Box"
+import Stack from "@mui/material/Stack"
+import styled from "@mui/material/styles/styled"
+
+export default function TopBar() {
+  return (
+    <Stack direction="row">
+      <div>One</div>
+      <div>Two</div>
+      <div>Three</div>
+    </Stack>
+  )
+}
