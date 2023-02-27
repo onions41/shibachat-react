b3e42e9c68475aaade6eec8a438cef977459506a
onions41 <kblee89@outlook.com>
2023-02-27 12:41:09 -0800

    Nudged children to format layout perfectly

--- src/components/contacts/FriendCard.jsx
+++ src/components/contacts/FriendCard.jsx
@@ -7,18 +7,23 @@ import MoreVertIcon from "@mui/icons-material/MoreVert"
 import formatDateShort from "utility/formatDateShort"
 
 export default function FriendCard({ friend }) {
+  // Grabs the lastest message between me and friend
+  // If there was no message yet, the default string in the line below
   let msgStr = "Send this inu a message"
   if (friend.latestMessageWithMe) {
     const createdAtDateObj = new Date(
       parseInt(friend.latestMessageWithMe.createdAt, 10)
     )
-    msgStr = `${formatDateShort(createdAtDateObj)}: ${friend.latestMessageWithMe.textContent}`
+    msgStr = `${formatDateShort(createdAtDateObj)}: ${
+      friend.latestMessageWithMe.textContent
+    }`
   }
 
   return (
+    // Out white box with rounded gray borders
     <Box
       sx={{
-        height: "100px",
+        height: "86px",
         margin: "8px 0",
         backgroundColor: "background.white",
         borderWidth: "1px",
@@ -26,14 +31,15 @@ export default function FriendCard({ friend }) {
         borderColor: "border.gray",
         borderRadius: "8px",
         display: "grid",
-        gridTemplateColumns: "80px 1fr 50px",
-        gridTemplateRows: "50% 50%"
+        gridTemplateColumns: "66px 1fr 50px",
+        gridTemplateRows: "45% 55%"
       }}
     >
+      {/* Avatar */}
       <Avatar
         sx={{
-          width: 45,
-          height: 45,
+          width: 36,
+          height: 36,
           marginTop: "18px",
           gridRow: "span 2",
           justifySelf: "center"
@@ -41,35 +47,42 @@ export default function FriendCard({ friend }) {
         alt="#"
         src="#"
       />
+      {/* Nickname of the friend */}
       <Typography
         noWrap
         sx={{
           alignSelf: "end",
-          fontWeight: "bold",
-          fontSize: "1.2rem"
+          fontWeight: "bold"
         }}
       >
         {friend.nickname}
       </Typography>
+      {/* More button */}
       <IconButton
         aria-label="open menu"
-        // edge="end"
-        size="large"
-        sx={{ marginTop: "12px" }}
+        sx={{
+          width: "37.8px",
+          height: "37.8px",
+          justifySelf: "center",
+          position: "relative",
+          top: "4px",
+          left: "2px"
+        }}
       >
-        <MoreVertIcon fontSize="inherit" />
+        <MoreVertIcon />
       </IconButton>
+      {/* Truncated last message */}
       <Typography
         sx={{
           maxHeight: "100%",
-          paddingRight: "16px",
-          overflow: "clip",
-          fontSize: "0.9rem",
+          paddingRight: "26px",
+          fontSize: "0.85rem",
           fontStyle: "italic",
           alignSelf: "center",
           display: "-webkit-box",
-          WebLineClamp: "2",
           WebkitBoxOrient: "vertical",
+          WebkitLineClamp: "2",
+          overflow: "clip",
           gridColumn: "span 2"
         }}
       >
