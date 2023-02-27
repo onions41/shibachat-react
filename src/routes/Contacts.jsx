402bc58b6fafe6e28c01651b36e5ff56749c79e5
onions41 <kblee89@outlook.com>
2023-02-27 12:56:16 -0800

    Added received fReq carousel

--- src/routes/Contacts.jsx
+++ src/routes/Contacts.jsx
@@ -1,15 +1,17 @@
 // MUI
 import Container from "@mui/material/Container"
 import Box from "@mui/material/Box"
+import Typography from "@mui/material/Typography"
 import styled from "@mui/material/styles/styled"
 
 // Components
 import TopBar from "components/contacts/TopBar"
 import ContactCardsGrid from "components/contacts/ContactCardsGrid"
+import ReceivedFReqCarousel from "components/contacts/ReceivedFReqCarousel"
 
 const FriendsRouteContainer = ({ children }) => (
   <Container
-    maxWidth="lg"
+    maxWidth="md"
     disableGutters={true}
     sx={{ height: "100%", pt: 2 }}
   >
@@ -58,7 +60,24 @@ export default function Contacts({ meCalled, meLoading, meError, me }) {
         <TopBar />
       </TopBarContainer>
       <FriendCardsScrollBox>
-        <ContactCardsGrid me={me} />
+        {me.receivedFRequests.length ? (
+          <>
+            <Typography
+              variant="h3"
+              mt={2}
+            >
+              Friend Requests
+            </Typography>
+            <ReceivedFReqCarousel me={me} />
+          </>
+        ) : null}
+        <Typography
+          variant="h3"
+          mt={2}
+        >
+          Friends
+        </Typography>
+        {me.friends.length ? <ContactCardsGrid me={me} /> : "You don't have any friends yet."}
       </FriendCardsScrollBox>
     </FriendsRouteContainer>
   )
